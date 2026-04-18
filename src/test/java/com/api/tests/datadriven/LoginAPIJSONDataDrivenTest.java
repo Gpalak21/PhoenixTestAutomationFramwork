fa98040189c4292a318c8279b0ea1760cc3207f9
Palak <guptapalakagra@gmail.com>
2026-04-18 14:03:04 +0530

    Created JSON Reader Utility for reading the login API test data from Json file

---
+++ src/test/java/com/api/tests/datadriven/LoginAPIJSONDataDrivenTest.java
@@ -0,0 +1,43 @@
+package com.api.tests.datadriven;
+
+import static com.api.utils.ConfigManager.getProperty;
+import static com.api.utils.SpecUtil.requestSpec;
+import static com.api.utils.SpecUtil.*;
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.io.IOException;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.api.request.model.UserCredentials;
+import com.dataproviders.api.bean.UserPOJO;
+
+public class LoginAPIJSONDataDrivenTest {
+	
+	
+	@Test(description="Verifying if login API is working for user FD", 
+			groups= {"api","regression","smoke","csv"},
+			dataProviderClass = com.dataproviders.DataProviderUtils.class,
+			dataProvider = "LoginAPIJsonDataProvider"
+			)
+	public void loginAPITest(UserCredentials userCreds) throws IOException{
+			
+		given()
+			.spec(requestSpec(userCreds))
+		.when()
+			.post("/login")
+		.then()
+			.spec(responseSpec_OK())
+			.body("message", equalTo("Success"))
+			.body(matchesJsonSchemaInClasspath("response-schema/LoginResponseSchema.json"));
+			
+	}
+	
+	
+	
+
+}
--- src/test/java/com/dataproviders/DataProviderUtils.java
+++ src/test/java/com/dataproviders/DataProviderUtils.java
@@ -7,9 +7,11 @@ import java.util.List;
 import org.testng.annotations.DataProvider;
 
 import com.api.request.model.CreateJobPayload;
+import com.api.request.model.UserCredentials;
 import com.api.utils.CSVReaderUtil;
 import com.api.utils.CreateJobBeanMapper;
 import com.api.utils.FakerDataGenerator;
+import com.api.utils.JsonReaderUtil;
 import com.dataproviders.api.bean.CreateJobBean;
 import com.dataproviders.api.bean.UserPOJO;
 
@@ -27,6 +29,12 @@ public class DataProviderUtils {
 		int fakerCountInt = Integer.parseInt(fakerCount); 
 		Iterator<CreateJobPayload> payloadIterator=FakerDataGenerator.generateFakeCreateJobData(fakerCountInt);
 		return payloadIterator;
+	}
+	
+	@DataProvider(name="LoginAPIJsonDataProvider", parallel=true)
+	public static Iterator<UserCredentials> loginAPIJsonDataProvider() {
+		return JsonReaderUtil.loadJSON("testData/loginApiTestData.json",UserCredentials[].class);
+		
 	}
 	
 
