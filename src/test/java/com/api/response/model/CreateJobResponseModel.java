b64ed496da475026de2728af5890cbfb74823e47
Palak <guptapalakagra@gmail.com>
2026-05-04 09:40:26 +0530

    Did the deserialization of createjob response to get different data from response

---
+++ src/test/java/com/api/response/model/CreateJobResponseModel.java
@@ -0,0 +1,18 @@
+package com.api.response.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+public class CreateJobResponseModel {
+	
+	
+	   String message; 
+	   CreateJobDataModel data;
+	        
+	    
+
+}
